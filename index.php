1c1a1975461170d35a032ca543adb588eba1e214
cawest1221 <cawest1221@gmail.com>
2014-03-21 12:42:37 -0400

    Auto refresh page on js script update

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+<title>Google Maps - pygmaps </title>
+
+<style type="text/css">
+      html { height: 100% }
+      body { height: 100%; margin: 0; padding: 0 }
+      #map_canvas { height: 100% }
+</style>
+<script type="text/javascript" src="jsrefresh.js#js,notify"></script>
+<script type="text/javascript" src="map.js?<?php echo time();?>"></script>
+<script type="text/javascript">
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+      'callback=initialize';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
+</script>
+
+</head>
+<body>
+	<div id="map_canvas"></div>
+</body>
+</html>
